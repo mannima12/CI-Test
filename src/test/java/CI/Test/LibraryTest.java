2d6b1f444aebd90a070af07ec4c5e8ee68f5c141
markus.remplbauer <markus.remplbauer@gmx.at>
2020-08-04 07:42:59 +0200

    edit googleJavaFormat

--- src/test/java/CI/Test/LibraryTest.java
+++ src/test/java/CI/Test/LibraryTest.java
@@ -1,60 +1,56 @@
 /**
  * Copyright 2020 Dynatrace LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package CI.Test;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+
 import junit.framework.Assert;
+import org.junit.Test;
 
 public class LibraryTest {
-    @Test
-    public void testSum() {
-        int a = 2;
-        int b = 5;
-        Library classUnderTest = new Library();
-        int result = classUnderTest.sum(a, b);
-        if (result != a + b) {
-            Assert.fail();
-        }
+  @Test
+  public void testSum() {
+    int a = 2;
+    int b = 5;
+    Library classUnderTest = new Library();
+    int result = classUnderTest.sum(a, b);
+    if (result != a + b) {
+      Assert.fail();
     }
+  }
 
-    @Test
-    public void testMinus() {
-        Library classUnderTest = new Library();
-        Assert.assertEquals(0, classUnderTest.minus(2, 2));
-    }
+  @Test
+  public void testMinus() {
+    Library classUnderTest = new Library();
+    Assert.assertEquals(0, classUnderTest.minus(2, 2));
+  }
 
-    @Test
-    public void testDivide() {
-        Library classUnderTest = new Library();
-        Assert.assertEquals(2, classUnderTest.divide(6, 3));
-    }
+  @Test
+  public void testDivide() {
+    Library classUnderTest = new Library();
+    Assert.assertEquals(2, classUnderTest.divide(6, 3));
+  }
 
-    @Test
-    public void testMultiply() {
-        Library classUnderTest = new Library();
-        Assert.assertEquals(6*3, classUnderTest.multiply(6, 3));
-    }
+  @Test
+  public void testMultiply() {
+    Library classUnderTest = new Library();
+    Assert.assertEquals(6 * 3, classUnderTest.multiply(6, 3));
+  }
 
-    @Test(expected = ArithmeticException.class)
-    public void testDivideWillThrowExceptionWhenDivideOnZero() {
-        Library classUnderTest = new Library();
-        classUnderTest.divide(6, 0);
-    }
-
+  @Test(expected = ArithmeticException.class)
+  public void testDivideWillThrowExceptionWhenDivideOnZero() {
+    Library classUnderTest = new Library();
+    classUnderTest.divide(6, 0);
+  }
 }
